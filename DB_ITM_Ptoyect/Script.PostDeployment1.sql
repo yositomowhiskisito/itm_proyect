5b32340c901522e19864520e9ec9f6bf77ce08e3
Alejandro <yositomowhiskysito@outlook.es>
2023-10-09 19:08:58 -0500

    Base data by db

--- DB_ITM_Ptoyect/Script.PostDeployment1.sql
+++ DB_ITM_Ptoyect/Script.PostDeployment1.sql
@@ -1,1 +1,13 @@
-﻿
+﻿IF NOT EXISTS (SELECT 1 FROM [dbo].[Persons] WHERE [Name] = 'PersonTest')
+BEGIN
+	INSERT INTO [dbo].[Persons]([SSN],[Name],[Born],[State],[File],[CreateDate],[UpdateDate])
+	VALUES(2879564,'PersonTest',GETDATE(), 1,NULL,GETDATE(), NULL)
+END
+GO
+
+IF NOT EXISTS (SELECT 1 FROM [dbo].[Users] WHERE [Email] = 'testUser@gmail.com')
+BEGIN
+	INSERT INTO [dbo].[Users]([Person],[State],[Email],[Password])
+	VALUES(1,1, 'testUser@gmail.com', '9876543210')
+END
+GO
